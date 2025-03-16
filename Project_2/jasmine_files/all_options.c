ef0989d4b81218a2c0299fa5a2c6055f18c76aca
ocasioj1 <ocasioj1@tcnj.edu>
2025-03-16 15:32:18 -0400

    added stats function which is having issues with option_3

--- Project_2/jasmine_files/all_options.c
+++ Project_2/jasmine_files/all_options.c
@@ -5,6 +5,8 @@
 #include <pthread.h>
 #include <sys/wait.h>
 #include <sys/mman.h>
+#include <time.h>
+#include <sys/stat.h>
 
 
 /* Global variables for board and line in board */
@@ -227,7 +229,7 @@ void *check_cols_mt(){
     for (int i = 0; i < 9; ++i){
         if (results_cols[i] == 0){
             /*At least one result was invalid*/
-            printf("lol: %d\n", i);
+            //printf("lol: %d\n", i);
             set_results(results_lock, &results_final[1], 0);
             pthread_exit(0);
         }
@@ -378,14 +380,6 @@ int option_2(){
 }
 
 int* shared_mem(){
-    // Naming shared memory
-	const char* name = "VALID";
-
-	// Size of shared memory
-	const int SIZE = 4096;
-
-	// Shared memory file descriptor
-	int shm_fd;
 	
 	// Memory map the pointer to shared memory object
     int* mem = mmap(NULL, sizeof(int) * 3, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -404,7 +398,7 @@ int option_3(){
         
     /* pass this as argument for check- functions */
     int* memory = shared_mem();
-
+    
     for(int i = 0; i<3; i++){
         pid = fork();
 
@@ -414,21 +408,24 @@ int option_3(){
                 //printf("checking rows\n");
                 pthread_create(&tid_row, NULL, check_rows_child, &memory[0]);
                 pthread_join(tid_row, NULL);
+                exit(0);
                         
             }
             else if(i==2){
                 //printf("checking cols\n");
                 pthread_create(&tid_col, NULL, check_cols_child, &memory[1]);
                 pthread_join(tid_col, NULL);
+                exit(0);
          
             }
             else{
                 //printf("checkings blocks\n");
                 pthread_create(&tid_block, NULL, check_blocks_child, &memory[2]);
                 pthread_join(tid_block, NULL);
+                exit(0);
          
             }
-            exit(0);
+            
         }
         else if(pid<0){
             if(i==1){
@@ -450,10 +447,13 @@ int option_3(){
     for(int i = 0; i < 3; ++i){
         if (memory[i] == 0){
             /*At least one result was invalid*/
+            munmap(memory, sizeof(int)*3); /* clean up shared mem*/
             return 0;
         }
     }
+
     /*All were valid!*/
+    munmap(memory, sizeof(int)*3); /* clean up shared mem*/
     return 1;
 }
 
@@ -499,58 +499,177 @@ int make_board(FILE* inputFile, int board[9][9], char line[80] ){
     return 0;
     
 }
+/** Thought process for t test:
+ * make 3 methods to run the method 50 times and take the average of all the times
+ * return the average (double)
+ * -- for graphs: print the data from each run into a csv file
+ * 
+ * in Excel:
+ * run the t tests using the given data in the CSV
+ * 
+ * null hyp: mean_option1 == mean option2 == mean option3 (there is no statistical difference)
+ * alt hyp: there is a difference 
+ * 
+ * graph the means of each run on separate lines (so three lines)-- peak may or may not be similar
+ * 
+ */
+
+// int run_stats(){
+
+//     FILE* fp = fopen("timing_results.csv", "w"); /* making 3 data collection files-- seeing if running each option first has any effect*/
+//     if (!fp) {
+//         perror("Failed to open file");
+//         return 1;
+//     }
+
+//     fprintf(fp, "Option,Run,Time(seconds),Mean Time(seconds)\n");
+//     clock_t start,finish;
+    
+//     for (int op = 1; op <= 3; op++) {
+//         double full_time = 0; // Total time for all runs in one option
+//         double timings[50]; // Array to store each run's timing
+
+//         for (int run = 0; run < 50; run++) {
+            
+//             start = clock();
+//             /* Choose option to run based on outter for loop */
+//             if (op == 1) {
+//                 option_1(); 
+//             } 
+//             else if (op == 2) {
+//                 option_2();
+//             } 
+//             else if (op == 3) {
+//                 option_3();
+//             }
+//             else{
+//                 perror("There was an error collecting data.\n");
+//             }
+
+//             finish = clock();
+//             double run_time = ((double)(finish - start)) / CLOCKS_PER_SEC;
+//             full_time += run_time;
+//             timings[run] = run_time;
+//         }
+        
+
+//         double mean = full_time / 50;
+
+//         /* Write each run and the mean time only on the last run of the set */
+//         for (int i = 0; i < 50; i++) {
+//             /* Only print mean on the last line for each option */
+//             if (i == 49) { 
+//                 //printf("%d,%d,%f,%f\n", op, i + 1, timings[i], mean);
+//                 fprintf(fp, "%d,%d,%f,%f\n", op, i + 1, timings[i], mean);
+//             } else {
+//                 fprintf(fp, "%d,%d,%f\n", op, i + 1, timings[i]);
+//             }
+//         }
+        
+        
+//     }
+
+//     fclose(fp);
+//     printf("Data collected. Please check timing_results.csv for results.\n");
+//     return 0;
+// }
+
+int run_stats(){
+    FILE* fp = fopen("timing_results.csv", "w"); /* making 3 data collection files-- seeing if running each option first has any effect*/
+    if (!fp) {
+        perror("Failed to open file");
+        return 1;
+    }
+
+    fprintf(fp, "Option,Run,Time(seconds),Mean Time(seconds)\n");
+    clock_t begin,end;
+    double full_time = 0;
+    double timings[50];
+
+    for(int run =0; run<50; run++){
+        begin = clock();
+        option_1();
+        end = clock();
+
+        double run_time = ((double)(end - begin)) / CLOCKS_PER_SEC;
+        
+        timings[run] = run_time;
+        fprintf(fp, "%d,%d,%f\n", 1, run + 1, timings[run]);
+    }
+
+    for(int run =0; run<50; run++){
+        begin = clock();
+        option_2();
+        end = clock();
+        
+
+        double run_time = ((double)(end - begin)) / CLOCKS_PER_SEC;
+        
+        timings[run] = run_time;
+        fprintf(fp, "%d,%d,%f\n", 2, run + 1, timings[run]);
+    }
+
+    // if you comment this for loop out, it prints option 1 and 2 to the csv perfectly.
+    for(int run =0; run<50; run++){
+        begin = clock();
+        option_3();
+        end = clock();
+        
+        double run_time = ((double)(end - begin)) / CLOCKS_PER_SEC;
+        
+        timings[run] = run_time;
+        fprintf(fp, "%d,%d,%f\n", 3, run + 1, timings[run]);
+    }
+    fclose(fp);
+    return 0;
+}
+
 
 
 int main(int argc, char** argv){
     /* Get option choice from user */
     int option = atoi(argv[1]);
+    //int option = 1;
     FILE* inputFile = fopen("input.txt", "r");
 
     make_board(inputFile, board, line);
-    
-    
 
     /* Execute chosen option and get timing */
     int verdict = 2;
-    time_t begin,end;
+    clock_t begin,end;
+    begin = clock();
     if(option == 1){
-        begin = time(NULL);
-        //printf("Begin time: %ld \n", begin);
         verdict = option_1();
-        end = time(NULL);
-        //printf("End time: %ld \n", end);
     }
     else if(option == 2){
-        begin = time(NULL);
-        //printf("Begin time: %ld \n", begin);
         verdict = option_2();
-        end = time(NULL);
-        //printf("End time: %ld \n", end);
     }
-
     else if(option == 3){
-        begin = time(NULL);
-        //printf("Begin time: %ld \n", begin);
         verdict = option_3();
-        end = time(NULL);
-        //printf("End time: %ld \n", end);
     }
     else{
         printf("Invalid option selected. Please select 1,2 or 3.\n");
     }
-    //print_arr3(results_final);
+    end = clock();
+
     /* Printing verdict with timing*/
+    double total_time = ((double) (end - begin)) / CLOCKS_PER_SEC;
     if(verdict == 1){
-        printf("SOLUTION: YES(%ld seconds)\n", end-begin);
+        printf("SOLUTION: YES(%lf seconds)\n", total_time);
     }
     else if(verdict == 0){
-        printf("SOLUTION: NO(%ld seconds)\n", end-begin);
+        printf("SOLUTION: NO(%lf seconds)\n", total_time);
     }
     else{
         printf("There was an error in determining the solution. \n");
     }
-    
-    //printf("%d\n", option_1());
+
+
+
+
+
+    return run_stats();
+
     return 0;
 }
 
